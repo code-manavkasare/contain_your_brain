3d7feeb613f323cf38da1959df80810773384b08
Manav Kasare <kmanav0705@gmail.com>
2022-07-28 23:03:09 +0530

    onboard ui

--- src/screens/Onboard.js
+++ src/screens/Onboard.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import {
-  Alert,
   Image,
   Platform,
   StyleSheet,
@@ -13,15 +12,20 @@ import Screen from '../components/Screen';
 import {sizes} from '../constants/sizes';
 
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
-import {colors} from '../constants/colors';
 import FaceId from '../assets/svg/FaceId';
 import Passcode from '../assets/svg/Passcode';
 import TouchId from '../assets/svg/TouchId';
+import {colors} from '../constants/colors';
 
 import TouchID from 'react-native-touch-id';
+
 import {useDispatch} from 'react-redux';
 import {authenticate} from '../redux/actions/auth';
 import {showToast} from '../services/toast';
+
+import ReactNativeBiometrics from 'react-native-biometrics';
+
+const rnBiometrics = new ReactNativeBiometrics({allowDeviceCredentials: true});
 
 export default function Onboard() {
   const dispatch = useDispatch();
@@ -37,8 +41,10 @@ export default function Onboard() {
       unifiedErrors: false,
       passcodeFallback: true,
     };
+
     TouchID.isSupported(optionalConfigObject)
       .then(biometryType => {
+        console.log(biometryType);
         if (biometryType === 'FaceID') {
           setFaceIdSupported(true);
         } else {
@@ -48,6 +54,8 @@ export default function Onboard() {
       .catch(error => {
         console.log(error);
       });
+    // const {biometryType} = await rnBiometrics.isSensorAvailable();
+    // console.log(biometryType);
   };
 
   const handleAuthenticate = () => {
@@ -85,6 +93,32 @@ export default function Onboard() {
       });
   };
 
+  const handlePasscode = () => {
+    rnBiometrics
+      .simplePrompt({promptMessage: 'Confirm fingerprint'})
+      .then(resultObject => {
+        const {success} = resultObject;
+
+        if (success) {
+          console.log('successful biometrics provided');
+        } else {
+          console.log('user cancelled biometric prompt');
+        }
+      })
+      .catch(() => {
+        console.log('biometrics failed');
+      });
+    // PasscodeAuth.authenticate('to demo this react-native component')
+    //   .then(success => {
+    //     // Success code
+    //     console.log(success);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //     // Failure code
+    //   });
+  };
+
   return (
     <Screen>
       <View style={styles.screen}>
@@ -114,14 +148,16 @@ export default function Onboard() {
               <Text style={styles.label}>Use</Text>
               <Text style={styles.label}>Face Recognition</Text>
             </View>
-            <View style={styles.container}>
-              <View style={styles.icon}>
-                <Passcode />
-              </View>
+            <TouchableWithoutFeedback onPress={handlePasscode}>
+              <View style={styles.container}>
+                <View style={styles.icon}>
+                  <Passcode />
+                </View>
 
-              <Text style={styles.label}>Use your</Text>
-              <Text style={styles.label}>Phone's Passcode</Text>
-            </View>
+                <Text style={styles.label}>Use your</Text>
+                <Text style={styles.label}>Phone's Passcode</Text>
+              </View>
+            </TouchableWithoutFeedback>
             <TouchableWithoutFeedback onPress={handleAuthenticate}>
               <View style={styles.container}>
                 <View style={styles.icon}>
