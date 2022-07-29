36d1ca943150af054481c3288494a13efeeb67b8
Manav Kasare <kmanav0705@gmail.com>
2022-07-29 11:37:18 +0530

    home screen button

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Image,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableWithoutFeedback,
@@ -12,67 +13,101 @@ import {sizes} from '../constants/sizes';
 import Paragraph from '../components/Paragraph';
 import {navigate} from '../services/navigation';
 
+import Animated, {SpringUtils} from 'react-native-reanimated';
+import {mix, useSpringTransition} from 'react-native-redash';
 import Feather from 'react-native-vector-icons/Feather';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Brain from '../assets/svg/Brain';
 
 export default function () {
   return (
-    <View style={styles.screen}>
-      <View style={[styles.row, {marginTop: sizes.padding}]}>
-        <VerticalTile type="setup" onPress={() => navigate('Setup')} />
-        <VerticalTile type="worries" onPress={() => navigate('Worries')} />
-      </View>
-      <View style={[{marginTop: sizes.padding}]}>
-        <HorizontalTile onPress={() => navigate('Tips')} />
-      </View>
+    <View style={{flex: 1, backgroundColor: colors.background}}>
+      <ScrollView
+        contentContainerStyle={{paddingBottom: sizes.padding * 4}}
+        style={styles.screen}>
+        <View style={[styles.row, {marginTop: sizes.padding}]}>
+          <VerticalTile type="setup" onPress={() => navigate('Setup')} />
+          <VerticalTile type="worries" onPress={() => navigate('Worries')} />
+        </View>
+        <View style={[{marginTop: sizes.padding}]}>
+          <HorizontalTile onPress={() => navigate('Tips')} />
+        </View>
 
-      <View style={styles.tilesContainer}>
-        <Tile style={styles.tile} text="Download my worries" download />
-        <Tile style={styles.tile} text="About Contain Your Brain" brain />
-        <Tile style={styles.tile} text="View instruction screens" eye />
-      </View>
+        <View style={styles.tilesContainer}>
+          <Tile style={styles.tile} text="Download my worries" download />
+          <Tile style={styles.tile} text="About Contain Your Brain" brain />
+          <Tile style={styles.tile} text="View instruction screens" eye />
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-const Tile = ({text, onPress, download, brain, eye}) => {
+const Tile = ({text, download, brain, eye}) => {
+  const [showMore, setShowMore] = useState(false);
+  const transition = useSpringTransition(showMore, {
+    ...SpringUtils.makeDefaultConfig(),
+    overshootClamping: true,
+    damping: new Animated.Value(20),
+  });
+  const containerHeight = mix(transition, 75, sizes.height * 0.3);
+  const rotateZ = mix(transition, 0, Math.PI / 2);
+
   return (
-    <TouchableWithoutFeedback onPress={onPress ? onPress : () => {}}>
-      <View style={[styles.tile]}>
-        <View style={{marginRight: sizes.padding}}>
-          {download && (
+    <TouchableWithoutFeedback onPress={() => setShowMore(!showMore)}>
+      <Animated.View
+        style={[
+          {
+            height: containerHeight,
+            borderBottomWidth: showMore ? 1 : 0,
+            borderBottomColor: colors.tileBorder,
+          },
+        ]}>
+        <View style={styles.tile}>
+          <View style={{marginRight: sizes.padding}}>
+            {download && (
+              <Feather
+                name="download"
+                color={colors.primary}
+                size={sizes.icon * 0.75}
+              />
+            )}
+            {brain && <Brain />}
+            {eye && (
+              <Feather
+                name="eye"
+                color={colors.primary}
+                size={sizes.icon * 0.75}
+              />
+            )}
+          </View>
+          <View style={{flex: 1}}>
+            <Paragraph>{text}</Paragraph>
+          </View>
+          <Animated.View style={{transform: [{rotateZ}]}}>
             <Feather
-              name="download"
-              color={colors.primary}
+              name="chevron-right"
+              color={colors.text}
               size={sizes.icon * 0.75}
             />
-          )}
-          {brain && (
-            <Brain />
-            // <MaterialCommunityIcons
-            //   name="brain"
-            //   color={colors.primary}
-            //   size={sizes.icon * 0.75}
-            // />
-          )}
-          {eye && (
-            <Feather
-              name="eye"
-              color={colors.primary}
-              size={sizes.icon * 0.75}
-            />
-          )}
+          </Animated.View>
         </View>
-        <View style={{flex: 1}}>
-          <Paragraph>{text}</Paragraph>
-        </View>
-        <Feather
-          name="chevron-right"
-          color={colors.text}
-          size={sizes.icon * 0.75}
-        />
-      </View>
+        {showMore && (
+          <View
+            style={{
+              marginTop: sizes.padding,
+              paddingHorizontal: sizes.padding,
+            }}>
+            <Text style={styles.paragraph}>
+              And don't worry about your worries,they are all safely stored
+              directly on your phone andcan only be securely accessed by you. No
+              account sign-up or Internet access is required. Access is via your
+              fingerprint or passcode only. Use our clinical psychologist's tips
+              to help you choose the best way to approach your worry and then
+              SOLVE, ACCEPT or REFLECT your way to worrying less & living more!
+            </Text>
+          </View>
+        )}
+      </Animated.View>
     </TouchableWithoutFeedback>
   );
 };
@@ -214,19 +249,23 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: colors.text,
   },
+  paragraph: {
+    fontSize: 12,
+    color: colors.text,
+  },
   tilesContainer: {
     marginTop: sizes.padding * 2,
     backgroundColor: colors.background,
     borderRadius: sizes.radius,
     elevation: 10,
+    shadowColor: colors.shadow,
   },
   tile: {
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     paddingVertical: sizes.padding,
     borderRadius: sizes.radius,
     paddingHorizontal: sizes.padding,
-    backgroundColor: colors.background,
     borderBottomWidth: 1,
     borderBottomColor: colors.tileBorder,
   },
